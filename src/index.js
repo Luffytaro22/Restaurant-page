ac5de7519a7321cc84b7758a952271301ec482dd
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-23 17:02:20 -0500

    Added click listeners to the sections

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import homePage from '../modules/home.js';
+import menuPage from '../modules/menu.js';
+import contactPage from '../modules/contact.js';
+
+const homeButton = document.querySelector('.nav-items:nth-child(1)');
+const menuButton = document.querySelector('.nav-items:nth-child(2)');
+const contactButton = document.querySelector('.nav-items:nth-child(3)');
+
+homeButton.addEventListener('click', homePage);
+menuButton.addEventListener('click', menuPage);
+contactButton.addEventListener('click', contactPage);
