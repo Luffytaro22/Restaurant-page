e5dd6c93167430b1ff834a9e95543a7379a78bfe
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-23 23:28:02 -0500

    Modified the listeners to add and remove the active class

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import './styles.css';
 import homePage from '../modules/home.js';
 import menuPage from '../modules/menu.js';
 import contactPage from '../modules/contact.js';
+import webIcon from './assets/puerta-torii.png';
+
+// Append web icon
+const link = document.createElement('link');
+link.href = webIcon;
+link.rel = 'icon';
+document.head.appendChild(link);
 
 const homeButton = document.querySelector('.nav-items:nth-child(1)');
 const menuButton = document.querySelector('.nav-items:nth-child(2)');
@@ -9,6 +16,21 @@ const contactButton = document.querySelector('.nav-items:nth-child(3)');
 
 homePage();
 
-homeButton.addEventListener('click', homePage);
-menuButton.addEventListener('click', menuPage);
-contactButton.addEventListener('click', contactPage);
+homeButton.addEventListener('click', () => {
+  homePage();
+  homeButton.classList.add('active');
+  menuButton.classList.remove('active');
+  contactButton.classList.remove('active');
+});
+menuButton.addEventListener('click', () => {
+  menuPage();
+  homeButton.classList.remove('active');
+  menuButton.classList.add('active');
+  contactButton.classList.remove('active');
+});
+contactButton.addEventListener('click', () => {
+  contactPage();
+  homeButton.classList.remove('active');
+  menuButton.classList.remove('active');
+  contactButton.classList.add('active');
+});
