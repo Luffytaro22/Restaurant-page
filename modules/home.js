340c7d818e6175d7c7c75221f056f19a2e7191c9
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-23 21:50:45 -0500

    Added a div container

--- modules/home.js
+++ modules/home.js
@@ -1,25 +1,27 @@
 const homePage = () => {
   const section = document.getElementById('main-section');
   section.innerHTML = `
-    <div>
-      <h1>Tokyo Sushi Bar</h1>
-    </div>
-    <div>
-      <p>“People judge things by their own experience, not knowing of the wide world outside.”</p>
-    </div>
-    <div>
-      <h2>Hours</h2>
-      <p>Monday: 8am - 8pm</p>
-      <p>Tuesday: 8am - 8pm</p>
-      <p>Wednesday: 8am - 8pm</p>
-      <p>Thursday: 8am - 8pm</p>
-      <p>Friday: 8am - 8pm</p>
-      <p>Saturday: 10am - 6pm</p>
-      <p>Sunday: 10am - 6pm</p>
-    </div>
-    <div>
-      <h3>Location</h3>
-      <p>742 Evergreen Terrace, Springfield</p>
+    <div class="container">
+      <div>
+        <h1>Tokyo Sushi Bar</h1>
+      </div>
+      <div>
+        <p>“People judge things by their own experience, not knowing of the wide world outside.”</p>
+      </div>
+      <div>
+        <h2>Hours</h2>
+        <p>Monday: 8am - 8pm</p>
+        <p>Tuesday: 8am - 8pm</p>
+        <p>Wednesday: 8am - 8pm</p>
+        <p>Thursday: 8am - 8pm</p>
+        <p>Friday: 8am - 8pm</p>
+        <p>Saturday: 10am - 6pm</p>
+        <p>Sunday: 10am - 6pm</p>
+      </div>
+      <div>
+        <h3>Location</h3>
+        <p>742 Evergreen Terrace, Springfield</p>
+      </div>
     </div>
   `;
 };
